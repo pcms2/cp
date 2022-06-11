217e63207b0dba3d8957af5abff823eab811cbc4
Sergei Tundykov <pcms2@yandex.com>
2022-06-11 20:13:44 +0300

    Добавлена база

---
+++ Z.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+void solve() {
+
+  
+
+  return;
+}
+
+signed main() {
+  ios_base::sync_with_stdio(0); cin.tie(0); // cout.precision(20); cout << fixed;
+#ifdef pcms2
+  freopen(".in", "r", stdin);
+  freopen(".out", "w", stdout);
+#endif
+
+  // int tt; cin >> tt; while(tt--)
+  solve(), cout << '\n';
+
+  return 0;
+}
